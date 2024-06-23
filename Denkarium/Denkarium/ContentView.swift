2966f7e08ffb24573b15ce9ddb40a66fbe769f76
David Brenn <davidbrenn@t-online.de>
2024-06-23 20:00:51 +0200

    Swift project init

---
+++ Denkarium/Denkarium/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Denkarium
+//
+//  Created by David Brenn on 23.06.24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ Denkarium/Denkarium/DenkariumApp.swift
@@ -0,0 +1,17 @@
+//
+//  DenkariumApp.swift
+//  Denkarium
+//
+//  Created by David Brenn on 23.06.24.
+//
+
+import SwiftUI
+
+@main
+struct DenkariumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
